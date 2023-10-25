8e0dd0afdb0f6cbcdb37d78f987ac4c0387d60b7
maabbas <mx38x@icloud.com>
2023-10-25 21:40:31 +0300

    idk

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	// "flag"
 	"fmt"
-	"os"
 	"log"
+	"os"
+	"strings"
 )
 
 var l_flag bool
@@ -11,56 +13,59 @@ var R_flag bool
 var a_flag bool
 var r_flag bool
 var t_flag bool
+var ls bool
 
-
-
-
-func main(){
+func main() {
 	pathname := validation()
-	listing(pathname)
-	if R_flag {
-		err := listFilesRecursively(pathname, "")
-		if err != nil {
-			log.Fatal(err)
-		}
+	if !l_flag && !r_flag && !R_flag && !a_flag && !t_flag {
+		listing(pathname)
 	}
+
+	// listing(pathname)
+	// fmt.Print(R_flag)
+	// if R_flag {
+	// 	err := listFilesRecursively(pathname, "")
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// }
 
 }
 
-func validation() (string){
-	l_flag, R_flag, a_flag, r_flag, t_flag, pathname := false, false, false, false, false, "."
+// func validation() (string,bool){
+// 	l_flag, R_flag, a_flag, r_flag, t_flag, pathname := false, false, false, false, false, "."
 
-	if len(os.Args) < 2 || os.Args[1] != "ls" {
-		fmt.Println("Error, read the manPage 1")
-		os.Exit(0)
-	} else if len(os.Args) == 3 && os.Args[2][0] == '-' {
-			for j :=1; j<len(os.Args[2]); j++ {
-				if !l_flag && os.Args[2][j] == 'l' {
-					l_flag = true
-				} else if !R_flag && os.Args[2][j] == 'R' {
-					R_flag = true
-				} else if !a_flag && os.Args[2][j] == 'a'{
-					a_flag = true
-				} else if !r_flag && os.Args[2][j] == 'r'{
-					r_flag = true
-				} else if !t_flag && os.Args[2][j] == 't'{
-					t_flag = true
-				} else {
-					fmt.Println("Error, read the manPage 2")
-					os.Exit(0)
-				}
-			}
-	} else if len(os.Args) !=2 {
-		fmt.Println("Error, read the manPage 3")
-		os.Exit(0)
-	}
+// 	if len(os.Args) < 2 || os.Args[1] != "ls" {
+// 		fmt.Println("Error, read the manPage 1")
+// 		os.Exit(0)
+// 	} else if len(os.Args) == 3 {
+// 				if !l_flag && os.Args[2] == "-l" {
+// 					l_flag = true
+// 				} else if !R_flag && os.Args[2] == "-R" {
+// 					R_flag = true
+// 				} else if !a_flag && os.Args[2] == "-a" {
+// 					a_flag = true
+// 				} else if !r_flag && os.Args[2] == "-r" {
+// 					r_flag = true
+// 				} else if !t_flag && os.Args[2] == "-t" {
+// 					t_flag = true
+// 				} else {
+// 					fmt.Println("Error, read the manPage 2")
+// 					os.Exit(0)
+// 				}
+// 				fmt.Print(R_flag)
+// 	} else if len(os.Args) !=2 {
+// 		fmt.Println("Error, read the manPage 3")
+// 		os.Exit(0)
+// 	}
+// 	fmt.Print(R_flag)
 
-	if len(os.Args) == 4 {
-		pathname = os.Args[3]
-	}
+// 	if len(os.Args) == 4 {
+// 		pathname = os.Args[3]
+// 	}
 
-	return pathname
-}
+// 	return pathname,R_flag
+// }
 
 func listing(dir string) {
 	file, err := os.Open(dir)
@@ -77,7 +82,7 @@ func listing(dir string) {
 	for _, fileInfo := range fileInfos {
 		if fileInfo.Name()[0] != '.' && !a_flag {
 			if fileInfo.IsDir() {
-				fmt.Printf("[%s]\n", fileInfo.Name())
+				fmt.Printf("%s/\n", fileInfo.Name())
 			} else {
 				fmt.Print(fileInfo.Name(), " ")
 			}
@@ -86,39 +91,90 @@ func listing(dir string) {
 	fmt.Println()
 }
 
-func listFilesRecursively(dirPath, indent string) error {
-	dir, err := os.Open(dirPath)
-	if err != nil {
-		return err
-	}
-	defer dir.Close()
+// func listFilesRecursively(dirPath, indent string) error {
+// 	dir, err := os.Open(dirPath)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer dir.Close()
 
-	fileInfos, err := dir.Readdir(-1)
-	if err != nil {
-		return err
-	}
+// 	fileInfos, err := dir.Readdir(-1)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	for _, fileInfo := range fileInfos {
-		fileName := fileInfo.Name()
+// 	for _, fileInfo := range fileInfos {
+// 		fileName := fileInfo.Name()
+// 		fileInfo.
+// 		if fileName != "." && fileName != ".." {
+// 			fmt.Print(indent)
+// 			if fileInfo.IsDir() {
+// 				fmt.Printf("%s \n ", fileName)
+// 				subDirPath := dirPath + "/" + fileName
+// 				subIndent := indent + "  "
+// 				err := listFilesRecursively(subDirPath, subIndent)
+// 				if err != nil {
+// 					return err
+// 				}
+// 			} else {
+// 				fmt.Println(fileName)
+// 			}
+// 		} else {
+// 			fmt.Print(indent)
 
-		if fileName != "." && fileName != ".." {
-			fmt.Print(indent)
-			if fileInfo.IsDir() {
-				fmt.Printf("[%s]\n", fileName)
-				subDirPath := dirPath + "/" + fileName
-				subIndent := indent + "  "
-				err := listFilesRecursively(subDirPath, subIndent)
-				if err != nil {
-					return err
+// 		}
+// 	}
+
+// 	return nil
+// }
+
+func validation() string {
+	//chek the argument
+	if len(os.Args) > 3 || len(os.Args) < 2 {
+		os.Exit(0)
+	} else if os.Args[1] != "ls" {
+		os.Exit(0)
+	} else if len(os.Args) == 2 {
+		ls = true
+	} else {
+		// spilt the args so maybe we have more than 1 flag
+		Flags := strings.Split(os.Args[2], " ")
+		// path name if it exist
+		PhathName := "./"
+		// for the flags range
+		for _, flag := range Flags {
+			if flag[0] == '-' {
+				// to check the ls flags
+				if len(flag) == 2 {
+					CheckFlag(rune(flag[1]))
+				} else {
+					for i := 1; i < len(flag); i++ {
+						CheckFlag(rune(flag[i]))
+					}
 				}
 			} else {
-				fmt.Println(fileName)
+				PhathName += flag
 			}
-		} else {
-			fmt.Print(indent)
-
 		}
+		return PhathName
 	}
+	return "./"
+}
 
-	return nil
+// will edit the flag
+func CheckFlag(c rune) {
+	if c == rune('l') {
+		l_flag = true
+	} else if c == rune('R') {
+		R_flag = true
+	} else if c == rune('a') {
+		a_flag = true
+	} else if c == rune('r') {
+		r_flag = true
+	} else if c == rune('t') {
+		t_flag = true
+	} else {
+		fmt.Print("unvslid input")
+		os.Exit(0)
+	}
 }
