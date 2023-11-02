83e6c5b7842b621f37973c04c4780edeaeec3de7
maabbas <mx38x@icloud.com>
2023-11-03 01:06:40 +0300

    ls&-a&-R&-r&-t 100% , -l 0%

--- main.go
+++ main.go
@@ -15,50 +15,18 @@ var a_flag bool
 var r_flag bool
 var t_flag bool
 var ls bool
-var pathname string
 
 func main() {
-	pathname = validation()
+	pathname := validation()
 	x := listing(pathname)
+	if t_flag {
+	x = SortByDate(x)
+	}
 	Print(pathname,x)
 
 
 }
 
-// func validation() (string,bool){
-// 	l_flag, R_flag, a_flag, r_flag, t_flag, pathname := false, false, false, false, false, "."
-
-// 	if len(os.Args) < 2 || os.Args[1] != "ls" {
-// 		fmt.Println("Error, read the manPage 1")
-// 		os.Exit(0)
-// 	} else if len(os.Args) == 3 {
-// 				if !l_flag && os.Args[2] == "-l" {
-// 					l_flag = true
-// 				} else if !R_flag && os.Args[2] == "-R" {
-// 					R_flag = true
-// 				} else if !a_flag && os.Args[2] == "-a" {
-// 					a_flag = true
-// 				} else if !r_flag && os.Args[2] == "-r" {
-// 					r_flag = true
-// 				} else if !t_flag && os.Args[2] == "-t" {
-// 					t_flag = true
-// 				} else {
-// 					fmt.Println("Error, read the manPage 2")
-// 					os.Exit(0)
-// 				}
-// 				fmt.Print(R_flag)
-// 	} else if len(os.Args) !=2 {
-// 		fmt.Println("Error, read the manPage 3")
-// 		os.Exit(0)
-// 	}
-// 	fmt.Print(R_flag)
-
-// 	if len(os.Args) == 4 {
-// 		pathname = os.Args[3]
-// 	}
-
-// 	return pathname,R_flag
-// }
 
 func listing(dir string) ([]fs.FileInfo) {
 	file, err := os.Open(dir)
@@ -76,16 +44,31 @@ func listing(dir string) ([]fs.FileInfo) {
 	
 }
 
+func SortByDate(filesInfos []fs.FileInfo) ([]fs.FileInfo) {
+		n := len(filesInfos)
+		for i := 0; i < n-1; i++ {
+			for j := 0; j < n-i-1; j++ {
+				if filesInfos[j].ModTime().Before(filesInfos[j+1].ModTime()) {
+					filesInfos[j], filesInfos[j+1] = filesInfos[j+1], filesInfos[j]
+				}
+			}
+		}
+	return filesInfos
+
+}
+
 
 func Print(path string , fileInfos []fs.FileInfo){
+	if a_flag && !r_flag {
+		fmt.Println("./  ../  ")
+	}
 	for i := 0 ; i < len(fileInfos) ; i++ {
 		index := i
 		if r_flag {
 			index = len(fileInfos)-i-1
 		}
-
 		fileInfo := fileInfos[index]
-		if fileInfo.Name()[0] != '.' || a_flag {
+		if fileInfo.Name()[0] != '.'|| a_flag  {
 			if fileInfo.IsDir() {
 				fmt.Printf("%s/  ", fileInfo.Name() )
 				if R_flag {
@@ -98,48 +81,11 @@ func Print(path string , fileInfos []fs.FileInfo){
 			}
 		}
 	}
-	
-	if a_flag {
-		// to print the i
+	if a_flag && r_flag {
 		fmt.Println("./  ../  ")
 	}
+	
 }
-
-// func listFilesRecursively(dirPath, indent string) error {
-// 	dir, err := os.Open(dirPath)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer dir.Close()
-
-// 	fileInfos, err := dir.Readdir(-1)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	for _, fileInfo := range fileInfos {
-// 		fileName := fileInfo.Name()
-// 		if fileName != "." && fileName != ".." {
-// 			fmt.Print(indent)
-// 			if fileInfo.IsDir() {
-// 				fmt.Printf("%s \n ", fileName)
-// 				subDirPath := dirPath + "/" + fileName
-// 				subIndent := indent + "  "
-// 				err := listFilesRecursively(subDirPath, subIndent)
-// 				if err != nil {
-// 					return err
-// 				}
-// 			} else {
-// 				fmt.Println(fileName)
-// 			}
-// 		} else {
-// 			fmt.Print(indent)
-
-// 		}
-// 	}
-
-// 	return nil
-// }
 
 func validation() string {
 	//chek the argument
@@ -191,3 +137,75 @@ func CheckFlag(c rune) {
 		os.Exit(0)
 	}
 }
+
+
+// func listFilesRecursively(dirPath, indent string) error {
+// 	dir, err := os.Open(dirPath)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer dir.Close()
+
+// 	fileInfos, err := dir.Readdir(-1)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	for _, fileInfo := range fileInfos {
+// 		fileName := fileInfo.Name()
+// 		if fileName != "." && fileName != ".." {
+// 			fmt.Print(indent)
+// 			if fileInfo.IsDir() {
+// 				fmt.Printf("%s \n ", fileName)
+// 				subDirPath := dirPath + "/" + fileName
+// 				subIndent := indent + "  "
+// 				err := listFilesRecursively(subDirPath, subIndent)
+// 				if err != nil {
+// 					return err
+// 				}
+// 			} else {
+// 				fmt.Println(fileName)
+// 			}
+// 		} else {
+// 			fmt.Print(indent)
+
+// 		}
+// 	}
+
+// 	return nil
+// }
+
+// func validation() (string,bool){
+// 	l_flag, R_flag, a_flag, r_flag, t_flag, pathname := false, false, false, false, false, "."
+
+// 	if len(os.Args) < 2 || os.Args[1] != "ls" {
+// 		fmt.Println("Error, read the manPage 1")
+// 		os.Exit(0)
+// 	} else if len(os.Args) == 3 {
+// 				if !l_flag && os.Args[2] == "-l" {
+// 					l_flag = true
+// 				} else if !R_flag && os.Args[2] == "-R" {
+// 					R_flag = true
+// 				} else if !a_flag && os.Args[2] == "-a" {
+// 					a_flag = true
+// 				} else if !r_flag && os.Args[2] == "-r" {
+// 					r_flag = true
+// 				} else if !t_flag && os.Args[2] == "-t" {
+// 					t_flag = true
+// 				} else {
+// 					fmt.Println("Error, read the manPage 2")
+// 					os.Exit(0)
+// 				}
+// 				fmt.Print(R_flag)
+// 	} else if len(os.Args) !=2 {
+// 		fmt.Println("Error, read the manPage 3")
+// 		os.Exit(0)
+// 	}
+// 	fmt.Print(R_flag)
+
+// 	if len(os.Args) == 4 {
+// 		pathname = os.Args[3]
+// 	}
+
+// 	return pathname,R_flag
+// }
