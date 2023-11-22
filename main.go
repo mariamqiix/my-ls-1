ff1b3c15afe893ed9382f6389da20b591b38b2ec
Mariam Abbas <maabbas@noreply.learn.reboot01.com>
2023-11-22 17:51:31 +0000

    Update 'main.go'

--- main.go
+++ main.go
@@ -4,5 +4,8 @@ import "my-ls/Myls"
 
 func main() {
 	pathname,subFile := Myls.Validation()
+	if l_flag && !SubFile_flag {
+		fmt.println(pathname,": ")
+	}
 	Myls.Print(pathname,subFile, Myls.Listing(pathname))
 }
