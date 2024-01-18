e586823bc3f04a24051dbbacd394e81dc3453ab0
Mariam Abbas <maabbas@noreply.learn.reboot01.com>
2024-01-18 14:15:26 +0000

    Update 'Myls/sort.go'

--- Myls/sort.go
+++ Myls/sort.go
@@ -9,7 +9,7 @@ func SortByAlph(filesInfos []fs.FileInfo) []fs.FileInfo {
 	n := len(filesInfos)
 	for i := 0; i < n-1; i++ {
 		for j := 0; j < n-i-1; j++ {
-			if filesInfos[j].Name() > filesInfos[j+1].Name() {
+			if strings.ToLower(filesInfos[j].Name()) > strings.ToLower(filesInfos[j+1].Name()) {
 				filesInfos[j], filesInfos[j+1] = filesInfos[j+1], filesInfos[j]
 			}
 		}
